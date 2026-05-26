2a85b9f29eb2fc20b06263e38f54800e4be631c9
Andres <andres_rdz92@hotmail.com>
2026-05-26 08:55:58 -0600

    primer deploy de Elyndor

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white">
+      <div className="text-center space-y-4">
+        <h1 className="text-4xl font-bold">
+          Elyndor 🌍
+        </h1>
+
+        <p className="text-zinc-400">
+          El mundo está despertando...
+        </p>
+      </div>
+    </div>
+  );
+}
